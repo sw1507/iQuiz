7697cb3cf54f42a1d1893fe58c9fcfb94db8cfbe
YuanShaochen <sw1507@uw.edu>
2017-11-15 22:59:17 -0800

    iQuiz2 first commit

--- iQuiz/iQuiz/SubjectListTBV.swift
+++ iQuiz/iQuiz/SubjectListTBV.swift
@@ -45,19 +45,43 @@ class SubjectListTBV: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return subjectList.count
+        return 3
     }
 
-    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var height:CGFloat = CGFloat()
+            height = 220
+        
+        return height
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-
-        // Configure the cell...
-        let s = subjectList[indexPath.row] as Subject
         
+        // Configure the cell...
+        
+        let s = subjectList[indexPath.row] as Subject
+       
+        //cell content
         cell.textLabel?.text = s.subjectName
         cell.detailTextLabel?.text = s.subjectDesc
         cell.imageView?.image = UIImage(named:s.subjectImageName)
+        
+        //font sizes configurations
+        cell.textLabel?.font = UIFont(name: "Arial", size:25)
+        cell.textLabel?.numberOfLines = 2
+        cell.detailTextLabel?.font = UIFont(name: "Arial", size:18)
+        cell.detailTextLabel?.numberOfLines = 3
+        //cell.detailTextLabel.lineBreakMode = UILineBreakModeWordWrap;
+        //image size configurations
+        let itemSize = CGSize(width:120.0, height:120.0)
+        UIGraphicsBeginImageContextWithOptions(itemSize, false, 0.0)
+        let imageRect = CGRect(x:0.0, y:0.0, width:itemSize.width, height:itemSize.height)
+        cell.imageView?.image!.draw(in:imageRect)
+        cell.imageView?.image! = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        //cell.textLabel.numberOfLines = 0;
+ 
         return cell
     }
     
@@ -70,6 +94,12 @@ class SubjectListTBV: UITableViewController {
         
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        UserDefaults.standard.set(indexPath.row, forKey: "selectedRow")
+        UserDefaults.standard.set(0, forKey: "queNumber")
+        UserDefaults.standard.set(0, forKey: "rightNumber")
+        //UserDefaults.standard.integer(forKey: "selectedRow") = indexPath.row
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -104,7 +134,7 @@ class SubjectListTBV: UITableViewController {
         return true
     }
     */
-
+    
     /*
     // MARK: - Navigation
 
